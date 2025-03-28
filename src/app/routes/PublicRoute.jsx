7fb29a9d5ebebc3bbe007588674207b6b5c2d04b
NguyenVanThangzzzz <106645703+NguyenVanThangzzzz@users.noreply.github.com>
2025-03-28 21:47:36 +0700

    update

--- src/app/routes/PublicRoute.jsx
+++ src/app/routes/PublicRoute.jsx
@@ -1,28 +1,21 @@
 import ResetPasswordSuccessfullyPage from "@pages/ResetPasswordSuccessfully/ResetPasswordSuccessfullyPage.jsx";
-import ResetPasswordSuccessfullyLayout from "@pages/ResetPasswordSuccessfully/ResetPasswordSuccessfullyLayout.jsx";
-
 import { PublicLayout } from "@app/layout/PublicLayout";
-
 
 const PublicRoute = [
   {
     path:"/",
     element:<PublicLayout />,
-    children: [{
-      path: "login",
-      element: "login",
-    }]
-  },
-  {
-    path: "reset-password-success",
-    element: <ResetPasswordSuccessfullyLayout />,
     children: [
       {
-        path: "",
-        element: <ResetPasswordSuccessfullyPage />,
+        path: "login",
+        element: "login",
       },
-    ],
-  },
+      {
+        path: "reset-password-success",
+        element: <ResetPasswordSuccessfullyPage />,
+      }
+    ]
+  }
 ];
 
 export default PublicRoute;
