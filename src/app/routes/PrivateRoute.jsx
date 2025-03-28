7a62411678a0dadaf128330ae9d5ddce67bd268e
Quy Pham <quy.pham@stunited.vn>
2025-03-28 09:41:49 +0700

    chore: code base

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -5,10 +5,63 @@ const PrivateRoute = [
   {
     path: "/",
     element: <ProtectedRoute />,
+    breadcrumb: "Home",
     children: [
       {
         path: "",
         element: <div>Dashboard</div>,
+        breadcrumb: "Dashboard",
+      },
+      {
+        path: "class",
+        breadcrumb: "Class Management",
+        children: [
+          {
+            path: "",
+            element: <div>Class Management</div>,
+            breadcrumb: "",
+          },
+          {
+            path: "detail",
+            element: <div>Class Detail</div>,
+            breadcrumb: "Class Detail",
+          },
+          {
+            path: "session-detail",
+            breadcrumb: "Session Detail",
+            children: [
+              {
+                path: "",
+                element: <div>Session Detail</div>,
+                breadcrumb: "",
+              },
+              {
+                path: "student",
+                children: [
+                  {
+                  path:":id",
+                  children: [
+                    {
+                      path:"",
+                      element: <div>Student Detail</div>,
+                    },
+                    {
+                      path:"grade",
+                      element: <div>Student Grade</div>,
+                    },
+                  ]
+                },
+               ],
+                breadcrumb: "Student Detail",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "profile",
+        element: <div>Profile Page</div>,
+        breadcrumb: "Profile",
       },
     ],
   },
