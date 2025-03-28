e6fe34fdbbae1436b70535155e27713e8495601a
Quy Pham <quy.pham@stunited.vn>
2025-03-28 09:41:49 +0700

    chore: code base

--- src/app/routes/ProtectedRoute/ProtectedRoute.jsx
+++ src/app/routes/ProtectedRoute/ProtectedRoute.jsx
@@ -1,5 +1,73 @@
-import { Outlet } from "react-router-dom";
+import { Logo } from "@assets/images";
+import { Layout, Menu } from "antd";
+import {
+  Outlet,
+  useLocation,
+  useNavigate,
+  matchRoutes,
+  useParams,
+} from "react-router-dom";
+import { StepForwardOutlined } from "@ant-design/icons";
+import { Breadcrumb } from "../../components/Breadcrumb/Breadcrumb";
+import PrivateRoute from "../PrivateRoute";
+
+const { Header, Content } = Layout;
 
 export const ProtectedRoute = () => {
-  return <Outlet />;
+  const location = useLocation();
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  // Generate breadcrumb paths based on route matches
+  const routes = matchRoutes(PrivateRoute, location.pathname) || [];
+  const breadcrumbPaths = routes.map(({ route }) => ({
+    name: route.breadcrumb || "",
+    link: route.path
+      ? location.pathname.split(route.path)[0] + route.path
+      : null,
+    param: id,
+  }));
+
+  // Function to handle navigation
+  const navigateTo = (key) => {
+    switch (key) {
+      case "1":
+        navigate("/");
+        break;
+      case "2":
+        navigate("/class");
+        break;
+      case "3":
+        navigate("/profile");
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Layout>
+      <Header className="flex items-center h-[7rem] bg-white shadow-xl">
+        <img src={Logo} className="max-w-44" />
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ flex: 1, minWidth: 0, justifyContent: "center" }}
+          items={[
+            { key: "1", label: "Dashboard" },
+            { key: "2", label: "Class Management" },
+            { key: "3", label: "Profile" },
+          ]}
+          onClick={(e) => navigateTo(e.key)}
+        />
+        <StepForwardOutlined style={{ fontSize: "24px", cursor: "pointer" }} />
+      </Header>
+      <Content style={{ padding: "0 48px" }}>
+        <Breadcrumb paths={breadcrumbPaths} />
+        <Outlet />
+      </Content>
+    </Layout>
+  );
 };
