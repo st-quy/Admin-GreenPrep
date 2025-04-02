94e77623d0c7a7dea72b50fba3d4a9a68c98a72f
vipprotk11abc <bangbangtest123@gmail.com>
2025-04-02 15:40:10 +0700

    Hot fix

--- src/pages/grading/GradingPage.jsx
+++ src/pages/grading/GradingPage.jsx
@@ -1,14 +1,17 @@
 import { Assessment } from "@features/grading/ui/Assessment";
-import AssessmentComponent from "@features/grading/ui/assessmentScores";
+import AssessmentScores from "@features/grading/ui/AssessmentScores";
 
 export const GradingPage = () => {
+
   const data1 = {
     writing: [8, 3, 3, 15],
     speaking: [7, 4, 5, 10],
   };
+  
+
   return (
     <div className="px-[6rem] pb-[6rem]">
-      <AssessmentComponent data={data1} />
+      <AssessmentScores data={data1} />
       <hr className="mt-[42px] mb-[37px]" />
       <Assessment />
     </div>
