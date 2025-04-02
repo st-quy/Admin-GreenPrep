8fa787ae3465f83788ccc085c8e8b43f62276467
Armsss9988 <leanhminh098@gmail.com>
2025-04-02 12:02:39 +0700

    feat GP-140 Fetch API

--- src/features/session/ui/StudentSessionTable.jsx
+++ src/features/session/ui/StudentSessionTable.jsx
@@ -1,175 +1,179 @@
-import React, { useMemo, useCallback, useState, useEffect } from "react";
-import { Table, Select, Pagination } from "antd";
-import { useNavigate } from "react-router-dom";
+import React, { useMemo, useState, useEffect, useCallback } from "react";
+import { Table, Select, Pagination, Spin } from "antd";
 import { TableType, StatusType, LevelEnum } from "../constraint/TableEnum";
+import {
+  useSessionParticipants,
+  useStudentParticipants,
+} from "../hooks/useSession";
 
 const StudentSessionTable = ({
-  data,
+  id,
   type,
-  status = type === TableType.STUDENT ? undefined : "draft",
+  status = "draft",
   onAllQuestionGraded = () => {},
+  onNavigate,
+  onStudentClick = (data) => {},
 }) => {
-  const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
+  const [levels, setLevels] = useState({});
+  const { data, isLoading } =
+    type === TableType.SESSION
+      ? useSessionParticipants(id)
+      : useStudentParticipants(id);
+  const processedData = useMemo(() => {
+    return (data?.data || []).map((record) => ({
+      ...record,
+      Total:
+        (record.GrammarVocab || 0) +
+        (record.Listening || 0) +
+        (record.Reading || 0) +
+        (record.Speaking || 0) +
+        (record.Writing || 0),
+    }));
+  }, [data]);
 
-  const initialLevels = useMemo(
-    () =>
-      data.reduce((acc, cur) => {
-        acc[cur.studentId] = cur.level;
-        return acc;
-      }, {}),
-    [data]
-  );
-
-  const [levels, setLevels] = useState(initialLevels);
+  useEffect(() => {
+    setLevels(
+      processedData.reduce((acc, cur) => ({ ...acc, [cur.ID]: cur.Level }), {})
+    );
+  }, [processedData]);
 
   const checkIsAllQuestionGraded = useCallback(() => {
-    const allGraded = data.every(
-      (record) =>
-        record.speaking !== null &&
-        record.speaking !== "" &&
-        record.writing !== null &&
-        record.writing !== ""
-    );
+    if (!processedData.length) return;
 
-    const allLevelsSelected = Object.values(levels).every(
-      (level) => level !== undefined && level !== null && level !== ""
+    const allGraded = processedData.every(
+      (record) => record.Speaking && record.Writing
     );
+    const allLevelsSelected = Object.values(levels).every((level) => level);
 
     if (allGraded && allLevelsSelected) {
-      onAllQuestionGraded();
+      onAllQuestionGraded?.();
     }
-  }, [data, levels, onAllQuestionGraded]);
+  }, [processedData, levels]);
 
   useEffect(() => {
     if (type === TableType.SESSION && status !== StatusType.PUBLISHED) {
       checkIsAllQuestionGraded();
     }
-  }, [levels, data, checkIsAllQuestionGraded]);
-
-  const handleNavigate = useCallback(
-    (studentId, partNo) => {
-      navigate(`/edit-score/${studentId}/${partNo}`);
-    },
-    [navigate]
-  );
+  }, [checkIsAllQuestionGraded]);
 
   const onLevelChange = (key, value) => {
     setLevels((prev) => ({ ...prev, [key]: value }));
   };
 
-  const columns = useMemo(() => {
-    const commonColumns = [
-      { title: "GRAMMAR & VOCABULARY", dataIndex: "grammar", key: "grammar" },
-      { title: "LISTENING", dataIndex: "listening", key: "listening" },
-      { title: "READING", dataIndex: "reading", key: "reading" },
-      {
-        title: "SPEAKING",
-        dataIndex: "speaking",
-        key: "speaking",
-        render: (text, record) =>
-          type === TableType.SESSION && status !== StatusType.PUBLISHED ? (
-            <a
-              className="cursor-pointer underline text-[14px] hover:opacity-80"
-              onClick={() => handleNavigate(record.studentId, "speaking")}
-            >
-              {text && text !== "" ? text : "Ungraded"}
-            </a>
-          ) : (
-            <span className="text-[12px] text-[#637381]">
-              {text && text !== "" ? text : "Ungraded"}
-            </span>
-          ),
-      },
-      {
-        title: "WRITING",
-        dataIndex: "writing",
-        key: "writing",
-        render: (text, record) =>
-          type === TableType.SESSION && status !== StatusType.PUBLISHED ? (
-            <a
-              className="cursor-pointer underline text-[14px] hover:opacity-80"
-              onClick={() => handleNavigate(record.studentId, "writing")}
-            >
-              {text && text !== "" ? text : "Ungraded"}
-            </a>
-          ) : (
-            <span className="text-[12px] text-[#637381]">
-              {text && text !== "" ? text : "Ungraded"}
-            </span>
-          ),
-      },
-      { title: "TOTAL", dataIndex: "total", key: "total" },
-      {
-        title: "LEVEL",
-        dataIndex: "level",
-        key: "level",
-        render: (level, record) => (
-          <Select
-            value={levels[record.studentId] || undefined}
-            placeholder="Level"
-            disabled={
-              status === StatusType.PUBLISHED || type === TableType.STUDENT
-            }
-            onChange={(value) => onLevelChange(record.studentId, value)}
-            className="w-20"
+  const commonColumns = [
+    {
+      title: "GRAMMAR & VOCABULARY",
+      dataIndex: "GrammarVocab",
+      key: "GrammarVocab",
+    },
+    { title: "LISTENING", dataIndex: "Listening", key: "Listening" },
+    { title: "READING", dataIndex: "Reading", key: "Reading" },
+    {
+      title: "SPEAKING",
+      dataIndex: "Speaking",
+      key: "Speaking",
+      render: (text, record) =>
+        type === TableType.SESSION && status !== StatusType.PUBLISHED ? (
+          <a
+            onClick={() => onNavigate(record.ID, "speaking")}
+            className="cursor-pointer underline text-[14px] hover:opacity-80"
           >
-            {LevelEnum.map((lvl) => (
-              <Select.Option key={`${record.studentId}-${lvl}`} value={lvl}>
-                {lvl}
-              </Select.Option>
-            ))}
-          </Select>
+            {text || "Ungraded"}
+          </a>
+        ) : (
+          <span className="text-[14px] text-[#637381]">
+            {text || "Ungraded"}
+          </span>
         ),
-      },
-    ];
+    },
+    {
+      title: "WRITING",
+      dataIndex: "Writing",
+      key: "Writing",
+      render: (text, record) =>
+        type === TableType.SESSION && status !== StatusType.PUBLISHED ? (
+          <a
+            onClick={() => onNavigate(record.ID, "writing")}
+            className="cursor-pointer underline text-[14px] hover:opacity-80"
+          >
+            {text || "Ungraded"}
+          </a>
+        ) : (
+          <span className="text-[14px] text-[#637381]">
+            {text || "Ungraded"}
+          </span>
+        ),
+    },
+    { title: "TOTAL", dataIndex: "Total", key: "Total" },
+    {
+      title: "LEVEL",
+      dataIndex: "Level",
+      key: "Level",
+      render: (level, record) => (
+        <Select
+          value={levels[record.ID]}
+          placeholder="Level"
+          disabled={
+            status === StatusType.PUBLISHED || type === TableType.STUDENT
+          }
+          onChange={(value) => onLevelChange(record.ID, value)}
+          className="w-20"
+        >
+          {LevelEnum.map((lvl) => (
+            <Select.Option key={lvl} value={lvl}>
+              {lvl}
+            </Select.Option>
+          ))}
+        </Select>
+      ),
+    },
+  ];
 
+  const columns = useMemo(() => {
     if (type === TableType.SESSION) {
       return [
-        { title: "STUDENT NAME", dataIndex: "name", key: "name" },
+        {
+          title: "STUDENT NAME",
+          dataIndex: ["User", "fullName"],
+          key: "fullName",
+          render: (text, record) => (
+            <a
+              onClick={() => onStudentClick(record.ID)}
+              className="cursor-pointer underline text-[14px] hover:opacity-80"
+            >
+              {text}
+            </a>
+          ),
+        },
         ...commonColumns,
       ];
-    } else if (type === TableType.STUDENT) {
+    } else {
       return [
-        { title: "SESSION NAME", dataIndex: "name", key: "name" },
+        { title: "SESSION NAME", dataIndex: ["Session","sessionName"], key: "SessionID" },
         ...commonColumns,
       ];
     }
-    return [];
-  }, [type, status, handleNavigate, onLevelChange, levels]);
+  }, [type, status, levels]);
 
-  const processedData = useMemo(
-    () =>
-      data.map((record) => ({
-        ...record,
-        total:
-          (record.grammar || 0) +
-          (record.listening || 0) +
-          (record.reading || 0) +
-          (record.speaking || 0) +
-          (record.writing || 0),
-      })),
-    [data]
-  );
-  const paginatedData = useMemo(
-    () =>
-      processedData.slice((currentPage - 1) * pageSize, currentPage * pageSize),
-    [data, currentPage, pageSize]
-  );
+  const paginatedData = useMemo(() => {
+    return processedData.slice(
+      (currentPage - 1) * pageSize,
+      currentPage * pageSize
+    );
+  }, [processedData, currentPage, pageSize]);
+
+  if (isLoading) return <Spin />;
 
   return (
     <Table
       columns={columns}
-      dataSource={paginatedData.map((item, index) => ({
-        ...item,
-        key: `${type}-${item.studentId}-${index}`,
-      }))}
+      dataSource={paginatedData.map((item) => ({ ...item, key: item.ID }))}
       pagination={{
-        style: { marginRight: "32px" },
         current: currentPage,
-        pageSize: pageSize,
-        total: data.length,
+        pageSize,
+        total: processedData.length,
         showSizeChanger: true,
         pageSizeOptions: ["5", "10", "15", "20"],
         showTotal: (total, range) =>
@@ -179,9 +183,9 @@ const StudentSessionTable = ({
           setPageSize(size);
         },
       }}
+      bordered
       className="border border-gray-200 pagination w-full p-0 m-0 overflow-x-auto bg-none"
       rowClassName="text-center"
-      bordered
       scroll={{ x: 768 }}
       components={{
         header: {
