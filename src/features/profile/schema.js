c95b151e11d1992380d120a50abfaa66b169eb98
thangtuoivui <49915120+thangtuoivui@users.noreply.github.com>
2025-04-10 15:35:36 +0700

    change schema login and change password

--- src/features/profile/schema.js
+++ src/features/profile/schema.js
@@ -3,10 +3,10 @@ import * as Yup from "yup";
 export const ChangePasswordSchema = Yup.object().shape({
   currentPassword: Yup.string()
     .required("Current password is required")
-    .min(6, "Current password must be at least 6 characters"),
+    .min(8, "Current password must be at least 8 characters"),
   newPassword: Yup.string()
     .required("New password is required")
-    .min(6, "New password must be at least 6 characters"),
+    .min(8, "New password must be at least 8 characters"),
   newPasswordConfirmation: Yup.string()
     .oneOf([Yup.ref("newPassword"), null], "Passwords must match")
     .required("New password confirmation is required"),
